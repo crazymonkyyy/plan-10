73a0411130711d9478c033fed1c9c5222c7d0279
monkyyy <crazymonkyyy@gmail.com>
2023-02-02 13:40:10 -0700

    updated

--- source/app.d
+++ source/app.d
@@ -3,26 +3,75 @@ import std;
 enum windowx=800;
 enum windowy=600;
 
-void main(string[] s){
+Color tocolor(string s){
+	import std.range;
+	Color output;
+	enum zippy= zip(
+		[16,1].cycle,
+		["r","r","g","g","b","b","a","a"],
+		iota(0,100));
+	static foreach(digit,mix,i;zippy){ {
+		int t;
+		if(s[i]>='0' && s[i]<='9'){
+			t=s[i]-'0';}
+		if(s[i]>='a' && s[i]<='f'){
+			t=s[i]-'a'+10;}
+		if(s[i]>='A' && s[i]<='F'){
+			t=s[i]-'A'+10;}
+		t*=digit;
+		mixin("output."~mix)+=t;
+	} }
+	return output;
+}
+
+void main(){
+	SetTraceLogLevel(int.max);
 	InitWindow(windowx, windowy, "Hello, Raylib-D!");
 	SetWindowPosition(2000,0);
-	("mkfifo "~s[1]).executeShell;
-	File data;
 	int y;
+	SetTargetFPS(60);
 	BeginDrawing();
 	ClearBackground(Colors.BLACK);
 	EndDrawing();
+	writeln("🕛");
+	stdout.flush;
+	auto log=File("log","w");
 	while (!WindowShouldClose()){
-		data.open(s[1]);
-		auto data_=data.byLineCopy;
-		if(data_.front==""){}
+		//log.writeln("in while loop"); log.flush;
 		BeginDrawing();
-			
-			foreach(a;data_){
-				DrawText(a.toStringz, 0,y, 20, Colors.WHITE);
-				y+=25;
-			}
-			//DrawFPS(10,10);
+			//log.writeln("started drawing"); log.flush;
+			//foreach(dstring a;data_.map!(a=>a.to!dstring)){try{
+			//if(!stdin.eof){
+				auto data_=readln.to!dstring;
+				log.writeln(data_); log.flush;
+				try{
+				dchar command=data_[0];
+				string[] rest=data_[1..$].split(' ').map!(a=>a.to!string).array;
+				switch(command){
+					case '🟥':
+						DrawPixel(rest[0].to!int,rest[1].to!int,rest[2].tocolor);
+						break;
+					case ' ':
+						ClearBackground(rest[1].tocolor);
+						break;
+					case '⚫':
+						DrawCircle(rest[0].to!int,rest[1].to!int,rest[2].to!float,rest[3].tocolor);
+						break;
+					case '◾':
+						DrawRectangle(rest[0].to!int,rest[1].to!int,rest[2].to!int,rest[3].to!int,
+								rest[5].tocolor);
+						break;
+					case '🄰':
+						DrawText(rest[3..$-1].joiner(" ").to!string.toStringz
+								,rest[0].to!int,rest[1].to!int,rest[2].to!int,
+								rest[$-1].tocolor);
+						break;
+					default:
+				}
+			}catch(Throwable){}//}
+		//log.writeln("left if statement"); log.flush;
+		writeln("🕛");
+		stdout.flush;
 		EndDrawing();
 	}
 	CloseWindow();
